b8e84a33eb1a8e4f11b84673653831de2ca9b9af
Jesus Bossa <x@jesusbossa.dev>
2022-02-14 21:30:12 -0500

    chore: add manifest

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,6 +32,8 @@ const Home: NextPage = () => {
           content={t('head-description', { planet: `${t(planet.objectPrefix)} ${t(planet.objectName)}`, system: planet.objectPrefix })}
         />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
       </Head>
 
       <main>
